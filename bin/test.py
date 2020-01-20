26d24054c59b0ab0bc96f703dddb21641bfd4754
Justus Schwabedal <jschwabedal@gmail.com>
2020-01-20 16:15:54 +0100

    Begin refactor bin/test.py

--- bin/test.py
+++ bin/test.py
@@ -1,23 +1,8 @@
-import os
-import sys
+#!/usr/bin/env python
+
 import argparse
-import logging
-import json
-import time
-from easydict import EasyDict as edict
-import torch
-import numpy as np
-from torch.utils.data import DataLoader
-from torch.nn import DataParallel
-import torch.nn.functional as F
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-
-from data.dataset import ImageDataset  # noqa
-from model.classifier import Classifier  # noqa
 
 parser = argparse.ArgumentParser(description='Test model')
-
 parser.add_argument('--model_path', default='./', metavar='MODEL_PATH',
                     type=str, help="Path to the trained models")
 parser.add_argument('--in_csv_path', default='dev.csv', metavar='IN_CSV_PATH',
@@ -29,9 +14,28 @@ parser.add_argument('--num_workers', default=8, type=int, help="Number of "
                     "workers for each data loader")
 parser.add_argument('--device_ids', default='0', type=str, help="GPU indices "
                     "comma separated, e.g. '0,1' ")
+opt = parser.parse_args()
+device_ids = list(map(int, opt.device_ids.split(',')))
 
-if not os.path.exists('test'):
-    os.mkdir('test')
+from os import makedirs
+from os.path import dirname, abspath, join
+from sys import path
+path.insert(0, '.')
+
+import json
+import time
+from easydict import EasyDict as edict
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from torch.nn import DataParallel
+import torch.nn.functional as F
+
+from data.dataset import ImageDataset  # noqa
+from model.classifier import Classifier  # noqa
+
+
+makedirs('test', exist_ok=True)
 
 
 def get_pred(output, cfg):
@@ -83,44 +87,31 @@ def test_epoch(cfg, args, model, dataloader, out_csv_path):
                 batch = ','.join(map(lambda x: '{}'.format(x), pred[:, i]))
                 result = path[i] + ',' + batch
                 f.write(result + '\n')
-                logging.info('{}, Image : {}, Prob : {}'.format(
-                    time.strftime("%Y-%m-%d %H:%M:%S"), path[i], batch))
+                print(f"Image: {path[i]}, Prob: {batch}")
 
 
-def run(args):
-    with open(args.model_path + 'cfg.json') as f:
-        cfg = edict(json.load(f))
 
-    device_ids = list(map(int, args.device_ids.split(',')))
-    num_devices = torch.cuda.device_count()
-    if num_devices < len(device_ids):
-        raise Exception(
-            '#available gpu : {} < --device_ids : {}'
-            .format(num_devices, len(device_ids)))
-    device = torch.device('cuda:{}'.format(device_ids[0]))
+with open(join(opt.model_path, 'cfg.json'), 'r') as fp:
+    cfg = edict(json.load(fp))
 
-    model = Classifier(cfg)
-    model = DataParallel(model, device_ids=device_ids).to(device).eval()
-    ckpt_path = os.path.join(args.model_path, 'best.ckpt')
-    ckpt = torch.load(ckpt_path, map_location=device)
-    model.module.load_state_dict(ckpt['state_dict'])
+num_devices = torch.cuda.device_count()
+if num_devices < len(device_ids):
+    raise Exception(
+        '#available gpu : {} < --device_ids : {}'
+        .format(num_devices, len(device_ids)))
+device = torch.device('cuda:{}'.format(device_ids[0]))
 
-    dataloader_test = DataLoader(
-        ImageDataset(args.in_csv_path, cfg, mode='test'),
-        batch_size=cfg.dev_batch_size, num_workers=args.num_workers,
-        drop_last=False, shuffle=False)
+model = Classifier(cfg)
+model = DataParallel(model, device_ids=device_ids).to(device).eval()
+ckpt_path = join(opt.model_path, 'best.ckpt')
+ckpt = torch.load(ckpt_path, map_location=device)
+model.module.load_state_dict(ckpt['state_dict'])
 
-    test_epoch(cfg, args, model, dataloader_test, args.out_csv_path)
+dataloader = DataLoader(
+    ImageDataset(opt.in_csv_path, cfg, mode='test'),
+    batch_size=cfg.dev_batch_size, num_workers=opt.num_workers,
+    drop_last=False, shuffle=False)
 
-    print('Save best is step :', ckpt['step'], 'AUC :', ckpt['auc_dev_best'])
+test_epoch(cfg, opt, model, dataloader, opt.out_csv_path)
 
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-
-    args = parser.parse_args()
-    run(args)
-
-
-if __name__ == '__main__':
-    main()
+print(f"Save best is step:", ckpt['step'], 'AUC :', ckpt['auc_dev_best'])
